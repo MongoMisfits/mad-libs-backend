d443b1bec2d181ea340b457fd4013d1fc4903e60
poppan2 <ye_aung94@yahoo.com>
2021-11-22 10:47:44 -0800

    switch to async try

--- db/seed.js
+++ db/seed.js
@@ -2,13 +2,20 @@
 const Template = require("../models/template")
 const templateData = require('./seedData.json')
 
-Template.deleteMany({}).then(()=>{
-    Template.insertMany(templateData, (err)=>{
-        if(err){
-            console.log(err)
-        }else{
-            console.log('Templates Added!')
-        }
-        process.exit()
-    })
-})
+// Template.deleteMany({}).then(()=>{
+//     Template.insertMany(templateData, (err)=>{
+//         if(err){
+//             console.log(err)
+//         }else{
+//             console.log('Templates Added!')
+//         }
+//         process.exit()
+//     })
+// }).catch(err => console.log(err))
+
+const setUp = async ()=>{
+    await Template.deleteMany();
+    await Template.insertMany(templateData)
+    process.exit()
+}
+setUp()
